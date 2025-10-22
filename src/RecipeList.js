ae1b9fbe1ba8058bb4560ac7e2a85b886d3359ad
mvrylan <mvrylan@gmail.com>
2025-10-22 17:10:07 -0400

    RecipeList.js can now display the recipes list

--- src/RecipeList.js
+++ src/RecipeList.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function RecipeList() {
+function RecipeList({ recipes, RemoveRecipe }) {
   
   // TODO: Display the list of recipes using the structure of table that is provided.
   // TODO: Create at least one additional component that is used by this component.
@@ -11,12 +11,26 @@ function RecipeList() {
       <table>
         <thead>
           <tr>
-            <th></th>
+            <th>Name</th>
+            <th>Cuisine</th>
+            <th>Photo</th>
+            <th>Ingredients</th>
+            <th>Preparation</th>
             <th>Actions</th>
           </tr>
         </thead>
         <tbody>
-        
+          {recipes.map((recipe, index) => (
+            <tr>
+              <td>{recipe.name}</td>
+              <td>{recipe.cuisine}</td>
+              <td>
+                <img src={recipe.photo} style={{ maxWidth:"50px" }} />
+              </td>
+              <td>{recipe.ingredients}</td>
+              <td>{recipe.preparation}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
