bccf060b897332d23d7f6352e230bfdb0ba3b7ac
mvrylan <mvrylan@gmail.com>
2025-10-23 15:18:40 -0400

    removed null note from RecipeCard

--- src/RecipeCard.js
+++ src/RecipeCard.js
@@ -5,7 +5,7 @@ function RecipeCard({ recipe, removeRecipe }) {
     <>
       <td>{recipe.name}</td>
       <td>{recipe.cuisine}</td>
-      <td> {/* FIX NULL BY END OF PROJECT */}
+      <td>
         <img src={recipe.photo === "" ? null : recipe.photo} alt={"Photo Not Found"} style={{ maxWidth:"50px" }}></img>
       </td>
       <td>{recipe.ingredients}</td>
